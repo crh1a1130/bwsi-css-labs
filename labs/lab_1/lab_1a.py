5c67c87a4cb55ed93dc0848d4715401a398b4de1
crh1a1130 <crh1a1130@gmail.com>
2026-02-12 17:13:59 -0600

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "crh1a1130" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("sm棍哥")
 
 if __name__ == "__main__":
     main()
