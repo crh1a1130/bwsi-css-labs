9f95f3906be33fff64898e261a87a9d5c6346738
crh1a1130 <crh1a1130@gmail.com>
2026-02-12 19:01:11 -0600

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -3,6 +3,8 @@ lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+
+changes the robot speed from 5 to 3 in the header comment.
 """
 
 def main():
